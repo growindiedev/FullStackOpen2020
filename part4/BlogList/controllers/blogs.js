0a8379fe1146e26b00f07a3fbd02706a9229ed33
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-10-29 20:09:01 +0530

    added blogs to the controllers

---
+++ part4/BlogList/controllers/blogs.js
@@ -0,0 +1,26 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const logger = require('../utils/logger')
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+        logger.info(blogs)
+      })
+  })
+  
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+        logger.info(result)
+      })
+  })
+
+
+  module.exports = blogsRouter;
--- part4/BlogList/index.js
+++ part4/BlogList/index.js
@@ -5,39 +5,16 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-
-
-
-
-
-
-
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-      logger.info(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-      logger.info(result)
-    })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
