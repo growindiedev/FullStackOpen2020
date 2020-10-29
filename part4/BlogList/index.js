11c58359b66ff5c64cb1826f1e59dcbdd6e9392a
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-10-29 20:18:26 +0530

    seperated index.js and app.js, exercise finished

--- part4/BlogList/index.js
+++ part4/BlogList/index.js
@@ -1,18 +1,8 @@
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('build'))
-app.use('/api/blogs', blogsRouter)
 
 
 
