2c3e51b9ae41c8c138a32d433c2e851d810ed19e
Abhishek Ranjan <rabhishekranjan@gmail.com>
2020-10-29 19:33:55 +0530

    added Blog to models

---
+++ part4/BlogList/index.js
@@ -0,0 +1,44 @@
+const config = require('./utils/config')
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const Blog = require('./models/blog')
+
+
+
+
+
+
+
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+      logger.info(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+      logger.info(result)
+    })
+})
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
